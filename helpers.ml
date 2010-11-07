1ea59129f4ef277d320abd87c1629c99b3d29483
Mike McClurg <mike.mcclurg@gmail.com>
2010-11-07 13:39:54 +0000

    Finished zip function in helpers.ml

--- helpers.ml
+++ helpers.ml
@@ -1,26 +1,6 @@
 
-type regex =
-    Empty
-  | Singleton of char
-  | Kleene of regex
-  | Catenation of regex * regex
-  | Alternation of regex * regex ;;
-
-let rec string_of_regex r = match r with
-    Empty -> ""
-  | Singleton c -> Char.escaped c
-  | Kleene r -> (string_of_regex r) ^ "*"
-  | Catenation (r1, r2) -> (string_of_regex r1) ^ (string_of_regex r2)
-  | Alternation (r1, r2) -> (string_of_regex r1) ^ "|" ^ (string_of_regex r2) ;;
-
-(* let rec rmatch auto = function *)
-(*   | ''      -> auto.accepting *)
-(*   |  -> rmatch {(auto.transition hd); tl} ;; *)
-
-(* let fold_left_str =  *)
-
 module String = struct
-  
+
   include String
 
   let fold_right_str f c s =
@@ -37,14 +17,19 @@ end
 module List = struct
 
   include List
-    
-  let rec zip az bz = match (az,bz) with
-      ([],_)
-    | (_,[]) -> []
-    | ((a::az'),(b::bz')) -> (a,b) :: (zip az' bz')
 
+  let zip az bz =
+    let rec loop zz az bz = match (az,bz) with
+        ([],_)
+      | (_,[]) -> zz
+      | ((a::az'),(b::bz')) -> loop ((a,b)::zz) az' bz'
+    in rev (loop [] az bz)
 
-  let zip_tail az bz =
-    let rec loop zz 
+  (* let zip_tail az bz = *)
+  (*   let rec loop zz az bz = match (az,bz) with *)
+  (*       ([],_) *)
+  (*     | (_,[]) -> zz *)
+  (*     | ((a::az'),(b::bz')) -> loop (zz @ [(a,b)]) az' bz' *)
+  (*   in loop [] az bz *)
 
 end
